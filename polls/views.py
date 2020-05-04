31cb02c2b5d8c4be3a096e5b7ee6e054f5f8c59c
negiseijin <satoru6221@gmail.com>
2020-05-05 04:07:54 +0900

    Customize the admin form.

--- polls/views.py
+++ polls/views.py
@@ -15,11 +15,13 @@ from .models import Choice
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
+    
     def get_queryset(self):
-        """Return the last five published questions.(not includeing those set to be published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now).order_by('pub_date')[:5]
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -27,9 +29,9 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     def get_queryset(self):
         """
-        Excludes any questions that aren't pubished yet.
+        Excludes any questions that aren't published yet.
         """
-        raise Question.objects.filter(pub_dat__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
